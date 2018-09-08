c0d5f16030727be061910c908e38d7c92216a00b
Helfi Pangestu <hellfipangestu@gmail.com>
2018-09-08 10:40:33 +0700

    add debugging data

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -42,11 +42,14 @@ class Webhook extends CI_Controller {
     // log every event requests
     $this->tebakkode_m->log_events($this->signature, $body);
 
-    if(is_array($this->events['events'])){
-      foreach ($this->events['events'] as $event){
-        // your code here
-      }
-    }
+    // debuging data
+    file_put_contents('php://stderr', 'Body: '.$body);
+    
+    // if(is_array($this->events['events'])){
+    //   foreach ($this->events['events'] as $event){
+    //     // your code here
+    //   }
+    // }
 
   } // end of index.php
 
